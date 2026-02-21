04d636b5b32832e9d322adcbf676fec9562dc711
Nicolas <nicolasandreose@gmail.com>
2026-02-20 23:17:34 -0300

    feat: ensure values in cards are shown

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -23,34 +23,59 @@ const Cards = () => {
     queryKey: ["user-transactions", user?.id],
     queryFn: async () => {
       const meTransactions = await TransactionService.me({ from, to });
-      console.log(meTransactions);
       return meTransactions;
     },
   });
+
+  const profitsAmount =
+    userTransactions?.reduce((acumulator, currentValue) => {
+      if (currentValue.type === "EARNING") {
+        return acumulator + Number(currentValue.amount);
+      }
+      return acumulator;
+    }, 0) ?? 0;
+
+  const expensesAmount =
+    userTransactions?.reduce((acumulator, currentValue) => {
+      if (currentValue.type === "EXPENSE") {
+        return acumulator + Number(currentValue.amount);
+      }
+      return acumulator;
+    }, 0) ?? 0;
+
+  const investmentAmount =
+    userTransactions?.reduce((acumulator, currentValue) => {
+      if (currentValue.type === "INVESTMENT") {
+        return acumulator + Number(currentValue.amount);
+      }
+      return acumulator;
+    }, 0) ?? 0;
+
+  const walletAmount = profitsAmount - expensesAmount - investmentAmount;
 
   return (
     <div className="grid grid-cols-3">
       <div className="col-span-2 grid w-213 grid-cols-2 grid-rows-2 gap-4">
         <CardSection
           text="Wallet"
-          value="R$ 2.700,00"
+          value={`R$ ${walletAmount}`}
           icon={<WalletIcon className="h-5 w-5 text-white opacity-100" />}
         />
         <CardSection
-          text="Wallet"
-          value="R$ 2.700,00"
+          text="Profits"
+          value={`R$ ${profitsAmount}`}
           icon={<TrendingUpIcon className="text-primary h-5 w-5 opacity-100" />}
         />
         <CardSection
-          text="Wallet"
-          value="R$ 2.700,00"
+          text="Expenses"
+          value={`R$ ${expensesAmount}`}
           icon={
             <TrendingDownIcon className="h-5 w-5 text-red-500 opacity-100" />
           }
         />
         <CardSection
-          text="Wallet"
-          value="R$ 2.700,00"
+          text="Investment"
+          value={`R$ ${investmentAmount}`}
           icon={<PiggyBankIcon className="h-5 w-5 text-blue-500 opacity-100" />}
         />
       </div>
