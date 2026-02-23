a33e36e2f0898f8d2398fec6698170f28823de7c
Nicolas <nicolasandreose@gmail.com>
2026-02-23 20:05:14 -0300

    feat: add table transactions component

---
+++ src/components/TransactionsTable.jsx
@@ -0,0 +1,111 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useGetAllUserTransactions } from "@/hooks/data/Transactions";
+
+import { Card, CardContent, CardHeader } from "./ui/card";
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
+
+const TransactionsTable = () => {
+  const { data: userTransactions } = useGetAllUserTransactions();
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getUTCDate();
+    const month = date.toLocaleString("en-US", {
+      month: "long",
+      timeZone: "UTC",
+    });
+    const year = date.getUTCFullYear();
+    return `${day} ${month} ${year}`;
+  };
+
+  const capitalizeFirstLetter = (string) => {
+    if (string.length === 0) {
+      return "";
+    }
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  return (
+    <Card>
+      <CardHeader className="font-semibold">Transactions</CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-25">Title</TableHead>
+              <TableHead>Type</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead>Amount</TableHead>
+              <TableHead className="text-right"></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {userTransactions?.map((userTransaction) => (
+              <TableRow key={userTransactions?.id}>
+                <TableCell className="font-medium">
+                  {capitalizeFirstLetter(userTransaction?.name)}
+                </TableCell>
+                <TableCell>{userTransaction?.type}</TableCell>
+                <TableCell>{formatDate(userTransaction?.date)}</TableCell>
+                <TableCell>{userTransaction?.amount}</TableCell>
+                <TableCell className="text-right">ICONE</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default TransactionsTable;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import ButtonAddTransaction from "@/components/ButtonAddTransaction";
 import Cards from "@/components/Cards";
 import Header from "@/components/Header";
 import LoadingAuthentication from "@/components/LoadingAuthentication";
+import TransactionsTable from "@/components/TransactionsTable";
 import DateSelector from "@/components/ui/date-selector";
 import { useAuthContext } from "@/contexts/auth";
 
@@ -21,7 +22,7 @@ const HomePage = () => {
   return (
     <div className="h-screen w-screen">
       <Header />
-      <div className="px-8">
+      <div className="space-y-6 px-8">
         <div className="mt-8 mb-6 flex justify-between">
           <h2 className="text-2xl">Dashboard</h2>
           <div className="flex">
@@ -29,8 +30,8 @@ const HomePage = () => {
             <ButtonAddTransaction />
           </div>
         </div>
-
         <Cards />
+        <TransactionsTable />
       </div>
     </div>
   );
